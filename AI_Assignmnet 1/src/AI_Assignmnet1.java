ffedeb830fea975ccc462ce1822705604f57288e
Dihan <Dihan@Dihan-PC>
2015-02-24 13:22:47 +0200

    Test commit

--- AI_Assignmnet 1/src/AI_Assignmnet1.java
+++ AI_Assignmnet 1/src/AI_Assignmnet1.java
@@ -15,6 +15,7 @@ public class AI_Assignmnet1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Testing the commit and push");
     }
     
 }
