584068b24e66063e4131e04286dbf511c58857ed
user <user@INFBN019.up.ac.za>
2015-03-09 14:59:39 +0200

    Fix gui and steps

--- AI_Assignmnet 1/src/GUI.java
+++ AI_Assignmnet 1/src/GUI.java
@@ -2,6 +2,7 @@
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.LinkedList;
 import java.util.Random;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
@@ -26,9 +27,11 @@ public class GUI {
     public static JButton[] grid;
     public static boolean Turn;
     public static JButton selectedButton;
+    public static int AllowedNumSteps = 1;
     
     public void createGUI(int w , int h)
     {
+        Turn = true;
         System.out.println(w + " " + h);
         frame = new JFrame();
         
@@ -109,6 +112,7 @@ public class GUI {
                                 grid[k] = new JButton("");
                                 grid[k].setBackground(Color.WHITE);
                                 grid[k].putClientProperty("Colour", Colour.WHITE);
+                                grid[k].putClientProperty("Index", k);
                                 grid[k].setName(k + "");
                                 grid[k].setBorder(new LineBorder(Color.BLACK,1));
                                 if(colombs < dimentions / 2)
@@ -189,27 +193,98 @@ public class GUI {
     }
     
     
-    private static void clickedButton(String yolo)
+    private static void clickedButton(String indexOfClickedButton)
     {
-        if(selectedButton == null){
-            selectedButton = grid[Integer.parseInt(yolo)];
-            grid[Integer.parseInt(yolo)].setBorder(new LineBorder(Color.GREEN, 1));
+        LinkedList<JButton> vertical = new LinkedList<JButton>();
+        LinkedList<JButton> horizontal = new LinkedList<JButton>();
+        
+        if(selectedButton == null){       
+            selectedButton = grid[Integer.parseInt(indexOfClickedButton)];
+            if(Turn){
+                if(selectedButton.getBackground() != Color.red){
+                    selectedButton = null;
+                    return;
+                }                
+            }else{
+                if(selectedButton.getBackground() != Color.blue){
+                    selectedButton = null;
+                    return;
+                }   
+                
+            }
+            grid[Integer.parseInt(indexOfClickedButton)].setBorder(new LineBorder(Color.GREEN, 1));
             frame.repaint();
         }
         else{
+            int indexUp = Integer.parseInt(indexOfClickedButton);
+            int indexDown = Integer.parseInt(indexOfClickedButton);
+            
+            for(int k = 0; k < AllowedNumSteps ; k++){
+                indexUp -= dimentions;
+                    if(indexUp >= 0)
+                       vertical.add(grid[indexUp]);
+                indexDown += dimentions;
+                    if(indexDown < dimentions*dimentions)
+                        vertical.add(grid[indexDown]);
+                
+            }
+            
+            int indexLeft = Integer.parseInt(indexOfClickedButton);
+            int indexRight = Integer.parseInt(indexOfClickedButton)+1;
+            int steps = 0;
+            
+            while(indexLeft % dimentions != 0 && steps < AllowedNumSteps){
+                indexLeft--;
+                horizontal.add(grid[indexLeft]);
+                steps++;
+            }
+            
+            steps = 0;
+            while(indexRight % dimentions != 0 && steps < AllowedNumSteps){
+                horizontal.add(grid[indexRight]);
+                indexRight++;
+                steps++;
+            }
+            boolean contain = false;
+            
+            if(vertical.contains(selectedButton)) contain = true;
+            if(horizontal.contains(selectedButton)) contain = true;
+            
+            if(!contain) {
+                return;
+            }
+            
             if(Turn){
-                System.out.println(yolo);
-                grid[Integer.parseInt(yolo)].setBackground(Color.red);
+                grid[Integer.parseInt(indexOfClickedButton)].setBackground(Color.red);
                 Turn = false;
                 frame.repaint();
             }
             else{ 
-                grid[Integer.parseInt(yolo)].setBackground(Color.blue);
+                grid[Integer.parseInt(indexOfClickedButton)].setBackground(Color.blue);
                 Turn = true;
                 frame.repaint();
             }
+
+                        
+            for(int i = 0; i < grid.length; i++){
+                if(grid[i].getBackground() != Color.red && grid[i].getBackground() != Color.blue){
+                   grid[i].setBackground(Color.white); 
+                }
+            }
+                       
             selectedButton.setBorder(new LineBorder(Color.BLACK, 1));
+            selectedButton.setBackground(Color.white);
             selectedButton = null;
+            
+            for(int i = 0; i < grid.length; i++){
+                if(grid[i].getBackground() == Color.red ){
+                   colourCell(i,grid[i].getBackground(),Color.pink); 
+                }
+                if(grid[i].getBackground() == Color.blue){
+                   colourCell(i,grid[i].getBackground(),Color.cyan);                     
+                }
+            }
+            
             frame.repaint();
         }
     }
@@ -238,25 +313,22 @@ public class GUI {
         int  checkLeft = -1, checkRight = 1, checkTop = -dimentions, checkBottom = dimentions, checkDiagLeftTop = -dimentions -1 , checkDiagTopRight = -dimentions +1,
                 checkDiagBottomLeft = dimentions -1 , checkDiagBottomRight = dimentions +1;
         
-        if(checkLeft + index > 0)
+        if(checkLeft + index >= 0)
             allowedCellIndexes[0] = checkLeft + index;
         if(checkRight + index < dimentions * dimentions)
             allowedCellIndexes[1] = checkRight + index;
-        if(checkTop + index > 0)
+        if(checkTop + index >= 0)
             allowedCellIndexes[2] = checkTop + index;
         if(checkBottom + index < dimentions * dimentions)
             allowedCellIndexes[3] = checkBottom + index;
-        if(checkDiagLeftTop + index > 0)
+        if(checkDiagLeftTop + index >= 0)
             allowedCellIndexes[4] = checkDiagLeftTop + index;
-        if(checkDiagTopRight + index > 0)
+        if(checkDiagTopRight + index >= 0)
             allowedCellIndexes[5] = checkDiagTopRight + index;
         if(checkDiagBottomLeft + index < dimentions * dimentions)
             allowedCellIndexes[6] = checkDiagBottomLeft + index;
         if(checkDiagBottomRight + index < dimentions * dimentions)
             allowedCellIndexes[7] = checkDiagBottomRight + index;
-        
-        for(int k = 0 ; k < allowedCellIndexes.length ; k++)
-            System.out.println(allowedCellIndexes[k]  + " " + k);
         
         if(index % dimentions == 0){
             allowedCellIndexes[0] = -1;
